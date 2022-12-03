a6ccdccf5aef248e4b041e184971697fe49f0679
Nin Dichoso <nindichoso@gmail.com>
2022-12-03 23:00:58 +0800

    Store last on timestamp to dynamodb

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+const AWS = require("aws-sdk");
 const switchbot = require('./switchbot');
 const triggerWebhook = require('./triggerWebhook');
 
-let lastOnTimestamp = null;
 const autoOffThresholdInSeconds = process.env.AUTO_OFF_THRESHOLD_IN_HOURS * 3600;
+let lastOnTimestamp = null;
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async function(event) {
   try {
     const status = await switchbot.getStatus();
     console.debug(status);
     if (status === 'on') {
+      const botLastOnTimestamp = await dynamo
+          .get({
+            TableName: "home_automation",
+            Key: {
+              key: 'botLastOnTimestamp',
+            }
+          })
+          .promise();
+      lastOnTimestamp = botLastOnTimestamp?.value;
+      console.debug(`lastOnTimestamp: ${lastOnTimestamp}`);
+      
       if (!lastOnTimestamp) {
         lastOnTimestamp = Math.floor(new Date().getTime() / 1000);
+        await dynamo
+          .put({
+            TableName: "home_automation",
+            Item: {
+              key: 'heaterLastOnTimestamp',
+              value: lastOnTimestamp
+            }
+          })
+          .promise();
       }
       console.debug('lastOnTimestamp', lastOnTimestamp);
       const currentTime = Math.floor(new Date().getTime() / 1000);
@@ -20,15 +42,23 @@ exports.handler = async function(event) {
       if ((currentTime - lastOnTimestamp) >= autoOffThresholdInSeconds) {
         console.log('Turning off device');
         await switchbot.setStatus(false);
-        await triggerWebhook({ message: 'Heater turned off'});
+        await triggerWebhook({ text: 'Heater turned off'});
       // if it's turned on but within the threshold, just call the webhook
       } else {
-        await triggerWebhook({ message: 'Heater is on'});
+        await triggerWebhook({ text: 'Heater is on'});
         console.log('Webhook triggered');
       }
     // reset status tracking if it's turned off
     } else {
-      lastOnTimestamp = null;
+      await dynamo
+        .put({
+          TableName: "home_automation",
+          Item: {
+            key: 'heaterLastOnTimestamp',
+            value: null
+          }
+        })
+        .promise();
     }
   } catch(err) {
     console.error(err);
